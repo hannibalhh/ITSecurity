b487601dda6927721ac6dd37deb8b3dbd5eb766f
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2012-05-03 16:47:09 +0200

    eigenes modPow mit Tests sqaure and multiply noch fehlerhaft

--- ITSicherheit/src/Aufgabe2Diffie/TestModPow.java
+++ ITSicherheit/src/Aufgabe2Diffie/TestModPow.java
@@ -1,5 +1,44 @@
 package Aufgabe2Diffie;
 
+import java.math.BigInteger;
+
 public class TestModPow {
 
+	public static void main(String[] args) {
+		// generateN();
+		modPowTest();
+	}
+
+	public static void squareAndMultiplyMod(BigInteger base, BigInteger exp,BigInteger mod) {
+		BigInteger biggi = MathUtil.squareAndMultiply(base,exp).mod(mod);
+		System.out.println(biggi);
+	}
+
+	public static void modPowTestEasy(){
+		BigInteger base = BigInteger.valueOf(2);
+	    BigInteger exp = BigInteger.valueOf(7);
+		BigInteger mod = BigInteger.valueOf(5);
+		System.out.println("standardimpl: " + base.modPow(exp, mod));
+		squareAndMultiplyMod(base,exp,mod);			
+	}
+	
+	public static void modPowTest(){
+		BigInteger base = BigInteger.valueOf(2);
+	    BigInteger exp = BigInteger.valueOf(7);
+		BigInteger mod = BigInteger.valueOf(5);
+		System.out.println("standardimpl: " + base.modPow(exp, mod));
+		System.out.println(MathUtil.modPow(base, exp, mod));		
+	}
+	
+	public static void squareAndMultiply(BigInteger base, BigInteger exp) {
+		BigInteger biggi = MathUtil.squareAndMultiply(base,exp);
+		System.out.println(biggi);
+	}
+
+	public static void powTest(){
+		BigInteger base = BigInteger.valueOf(2);
+	    BigInteger exp = BigInteger.valueOf(7);
+		System.out.println("standardimpl: " + base.pow(exp.intValue()));
+		squareAndMultiply(base,exp);			
+	}
 }
