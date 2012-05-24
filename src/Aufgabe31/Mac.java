50c9dfb05bbf512c7f61f43a2b684649a0cba50d
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2012-05-24 22:41:51 +0200

    Aufgabe 31 läuft

--- src/Aufgabe31/Mac.java
+++ src/Aufgabe31/Mac.java
@@ -1,16 +1,13 @@
 package Aufgabe31;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-
 import CharReader.r;
 
 public class Mac {
 	
-	static final BigInteger key = BigInteger.valueOf(0x21735);
+	static final BigInteger key = BigInteger.valueOf(0x123);
 //	static final byte[] m = r.fileToByteArray("src/Aufgabe31/kafka-strafkolonie01.txt");
 	static final byte[] clearm = r.fileToByteArray("src/Aufgabe31/short");
-
 	
 	public static void main(String args[]){
 		System.out.println(Message.create(clearm, key));
--- src/Aufgabe31/Message.java
+++ src/Aufgabe31/Message.java
@@ -8,119 +8,55 @@ import java.util.List;
 
 public class Message {
 
-	private static int blockLength = 16; // byte
-	private final byte[] m;
+	private final Block m;
 	private final byte[] clearm;
 	private final BigInteger k;
-	
-	private Message(byte[] m,byte[] clearm,BigInteger k){
+
+	private Message(Block m, byte[] clearm, BigInteger k) {
 		this.m = m;
 		this.clearm = clearm;
 		this.k = k;
 	}
-	
-	public static Message create(byte[] clearm, BigInteger k){
-		return new Message(
-				mac(clearm,fillKey(k.toByteArray())),
-				clearm,
-				k);
+
+	public static Message create(byte[] clearm, BigInteger k) {
+		return new Message(mac(clearm, k), clearm, k);
 	}
-	
-	public static byte[] mac(byte[] clearm, byte[] filledk){ 
-		List<Block> blocks = blocks(clearm);
+
+	public static Block mac(byte[] clearm, BigInteger key) {
+		Block filledk = Block.fillKey(key.toString().getBytes());
+		List<Block> blocks = new ArrayList<Block>();
+		blocks.add(filledk);
+		blocks.addAll(Block.buildBlocks(clearm));
 		Iterator<Block> i = blocks.iterator();
-		Block a = null;
-		Block b = i.next();
-		while(i.hasNext()){
-			a = b;
-			a = Block.create(hash(a.value(),b.value()));
-			b = i.next();
+		Block a = i.next();
+		Block b = null;
+		while (i.hasNext()) {
+			b = a;
+			a = i.next();
+			a = a.hash(b);		
 		}
-		return a.value();
-	}	
-	
-	public static List<Block> blocks(byte[] blocks){
-		List<Block> l = new ArrayList<Block>();
-		byte[] temp = new byte[blockLength];
-		int i = 0;
-		for(byte b: blocks){
-			if(i % blockLength == 0){
-				i = 0;
-				temp = new byte[blockLength];
-			}
-			temp[i] = b;
-			l.add(Block.create(temp));
-			i++;
-		}
-		return l;
+		return a;
 	}
-	
-	public static byte[] fillKey(byte[] k){
-		if (k.length < blockLength){
-			byte[] array = new byte[blockLength];
-			for(int i = 0;i < blockLength;i++){
-				if (i < blockLength){
-					array[i] = k[i];
-				}
-				else{
-					array[i] = 0;
-				}
-			}
-			return array;
-		}
-		else if (k.length == blockLength){
-			return k;
-		}
-		else{
-			System.err.println("key to long: " + k.length);
-			return k;
-		}
-	}
-	
-	public static final byte intToByte(int value) {
-	    return (byte)value;
-	}
-	
-	public static byte[] hash(byte[] a, byte[] b){
-		if (a.length != b.length){
-			System.err.println("a:" + a.length +" != b:" + b.length);
-			System.exit(-1);
-		}
-		byte[] r = new byte[a.length];
-		for (int i=0;i<=a.length;i++){
-			r[i] = intToByte(a[i] ^ b[i]);
-		}
-		return r;
-	}
-	
-	public byte[] m(){
+
+	public Block m() {
 		return m;
 	}
-	
-	public byte[] clearm(){
+
+	public byte[] clearm() {
 		return clearm;
 	}
-	
-	public BigInteger k(){
+
+	public BigInteger k() {
 		return k;
 	}
-	
-	public static String print(byte[] array){
-		String s = "";
-		for (byte b:array){
-			s += (char)b;
-		}
-		return s;
-	}
-	
-	public static int blockLength(){
-		return blockLength;
+
+	public static int blockLength() {
+		return Block.length();
 	}
 
 	@Override
 	public String toString() {
-		return "Message(m=" + Arrays.toString(m) + ", clearm="
-				+ Arrays.toString(clearm) + ", k=" + k + ")";
+		return "Message(m=" + m + ", clearm=" + Block.print(clearm) + ", k=" + k + ")";
 	}
 
 	@Override
@@ -129,7 +65,7 @@ public class Message {
 		int result = 1;
 		result = prime * result + Arrays.hashCode(clearm);
 		result = prime * result + ((k == null) ? 0 : k.hashCode());
-		result = prime * result + Arrays.hashCode(m);
+		result = prime * result + ((m == null) ? 0 : m.hashCode());
 		return result;
 	}
 
@@ -149,10 +85,12 @@ public class Message {
 				return false;
 		} else if (!k.equals(other.k))
 			return false;
-		if (!Arrays.equals(m, other.m))
+		if (m == null) {
+			if (other.m != null)
+				return false;
+		} else if (!m.equals(other.m))
 			return false;
 		return true;
 	}
-	
-	
+
 }
