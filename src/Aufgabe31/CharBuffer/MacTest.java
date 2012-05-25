02964659ae4c7ae018b04927d57243d7faa45b88
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2012-05-25 21:54:27 +0200

    Aufgabe 31 mit Byte, Char und CharBuffer implementiert, kafka Bruteforce dauert zumindest sehr lange

--- src/Aufgabe31/CharBuffer/MacTest.java
+++ src/Aufgabe31/CharBuffer/MacTest.java
@@ -7,18 +7,19 @@ import CharReader.Interface.CharReader;
 public class MacTest {
 	
 	static final BigInteger key = BigInteger.valueOf(0x123);
-	static final CharReader clearm = r.file("src/Aufgabe31/kafka");
+//	static final CharReader clearm = r.file("src/Aufgabe31/kafka");
 //	static final char[] clearm = r.fileTocharArray("src/Aufgabe31/short");
-//	static final CharReader clearm = r.file("src/Aufgabe31/short");
+	static final CharReader clearm = r.file("src/Aufgabe31/short");
 	
 	public static void main(String args[]){
-//		easyTest();
-		bruteForceTest();
+		easyTest();
+//		bruteForceTest();
 	}
 	
 	public static void easyTest(){
 		Message m = Message.create(clearm, key);
-		System.out.println(m);
+		System.out.println(m.k());
+		System.out.println(m.m());
 	}	
 	
 	public static void bruteForceTest(){
@@ -29,5 +30,4 @@ public class MacTest {
 		System.out.println("tested Keys: "+bf.searchedKeys);
 		System.out.println("Duration in ms: "+bf.lastDurationMS);		
 	}
-
 }
